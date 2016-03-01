02c9ba4d0a57909ab6616ee9391e69a2dfa0b243
Victor Sigler <vikt0r.sigler@gmail.com>
2016-03-01 16:41:18 -0500

    Change the owner name

--- AccordionTableSwift/AppDelegate.swift
+++ AccordionTableSwift/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  AccordionTableSwift
 //
-//  Created by Victor on 2/4/15.
-//  Copyright (c) 2015 Pentlab. All rights reserved.
+//  Created by Victor Sigler on 2/4/15.
+//  Copyright (c) 2015 Private. All rights reserved.
 //
 
 import UIKit
