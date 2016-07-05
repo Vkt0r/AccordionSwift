d8404e78209c7dcce1a0cb4783d15c8f7db6164d
Victor Sigler <vikt0r.sigler@gmail.com>
2016-07-05 18:06:30 -0400

    Add sample project for purpose of show the cells

--- AccordionTableSwift/AccordionTableViewController.swift
+++ AccordionTableSwift/AccordionTableViewController.swift
@@ -2,8 +2,240 @@
 //  AccordionTableViewController.swift
 //  AccordionTableSwift
 //
-//  Created by Victor on 7/5/16.
-//  Copyright © 2016 Pentlab. All rights reserved.
+//  Created by Victor Sigler on 2/4/15.
+//  Copyright (c) 2015 Private. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AccordionTableViewController: UITableViewController {
+    
+    /// The number of elements in the data source
+    var total = 0
+    
+    /// The identifier for the parent cells.
+    let parentCellIdentifier = "ParentCell"
+    
+    /// The identifier for the child cells.
+    let childCellIdentifier = "ChildCell"
+    
+    /// The data source
+    var dataSource: [Parent]!
+    
+    /// Define wether can exist several cells expanded or not.
+    var numberOfCellsExpanded: NumberOfCellExpanded = .One
+    
+    /// Constant to define the values for the tuple in case of not exist a cell expanded.
+    let NoCellExpanded = (-1, -1)
+    
+    /// The index of the last cell expanded and its parent.
+    var lastCellExpanded : (Int, Int)!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.lastCellExpanded = NoCellExpanded
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    /**
+     Expand the cell at the index specified.
+     
+     - parameter index: The index of the cell to expand.
+     */
+    private func expandItemAtIndex(index : Int, parent: Int) {
+        
+        // the data of the childs for the specific parent cell.
+        let currentSubItems = self.dataSource[parent].childs
+        
+        // update the state of the cell.
+        self.dataSource[parent].state = .Expanded
+        
+        // position to start to insert rows.
+        var insertPos = index + 1
+        
+        let indexPaths = (0..<currentSubItems.count).map { _ -> NSIndexPath in
+            let indexPath = NSIndexPath(forRow: insertPos, inSection: 0)
+            insertPos += 1
+            return indexPath
+        }
+        
+        // insert the new rows
+        self.tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Fade)
+        
+        // update the total of rows
+        self.total += currentSubItems.count
+    }
+    
+    /**
+     Collapse the cell at the index specified.
+     
+     - parameter index: The index of the cell to collapse
+     */
+    private func collapseSubItemsAtIndex(index : Int, parent: Int) {
+        
+        var indexPaths = [NSIndexPath]()
+        
+        let numberOfChilds = self.dataSource[parent].childs.count
+        
+        // update the state of the cell.
+        self.dataSource[parent].state = .Collapsed
+        
+        guard index + 1 <= index + numberOfChilds else { return }
+        
+        // create an array of NSIndexPath with the selected positions
+        indexPaths = (index + 1...index + numberOfChilds).map { NSIndexPath(forRow: $0, inSection: 0)}
+        
+        // remove the expanded cells
+        self.tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Fade)
+        
+        // update the total of rows
+        self.total -= numberOfChilds
+    }
+    
+    /**
+     Update the cells to expanded to collapsed state in case of allow severals cells expanded.
+     
+     - parameter parent: The parent of the cell
+     - parameter index:  The index of the cell.
+     */
+    private func updateCells(parent: Int, index: Int) {
+        
+        switch (self.dataSource[parent].state) {
+            
+        case .Expanded:
+            self.collapseSubItemsAtIndex(index, parent: parent)
+            self.lastCellExpanded = NoCellExpanded
+            
+        case .Collapsed:
+            switch (numberOfCellsExpanded) {
+            case .One:
+                // exist one cell expanded previously
+                if self.lastCellExpanded != NoCellExpanded {
+                    
+                    let (indexOfCellExpanded, parentOfCellExpanded) = self.lastCellExpanded
+                    
+                    self.collapseSubItemsAtIndex(indexOfCellExpanded, parent: parentOfCellExpanded)
+                    
+                    // cell tapped is below of previously expanded, then we need to update the index to expand.
+                    if parent > parentOfCellExpanded {
+                        let newIndex = index - self.dataSource[parentOfCellExpanded].childs.count
+                        self.expandItemAtIndex(newIndex, parent: parent)
+                        self.lastCellExpanded = (newIndex, parent)
+                    }
+                    else {
+                        self.expandItemAtIndex(index, parent: parent)
+                        self.lastCellExpanded = (index, parent)
+                    }
+                }
+                else {
+                    self.expandItemAtIndex(index, parent: parent)
+                    self.lastCellExpanded = (index, parent)
+                }
+            case .Several:
+                self.expandItemAtIndex(index, parent: parent)
+            }
+        }
+    }
+    
+    /**
+     Find the parent position in the initial list, if the cell is parent and the actual position in the actual list.
+     
+     - parameter index: The index of the cell
+     
+     - returns: A tuple with the parent position, if it's a parent cell and the actual position righ now.
+     */
+    private func findParent(index : Int) -> (parent: Int, isParentCell: Bool, actualPosition: Int) {
+        
+        var position = 0, parent = 0
+        guard position < index else { return (parent, true, parent) }
+        
+        var item = self.dataSource[parent]
+        
+        repeat {
+            
+            switch (item.state) {
+            case .Expanded:
+                position += item.childs.count + 1
+            case .Collapsed:
+                position += 1
+            }
+            
+            parent += 1
+            
+            // if is not outside of dataSource boundaries
+            if parent < self.dataSource.count {
+                item = self.dataSource[parent]
+            }
+            
+        } while (position < index)
+        
+        // if it's a parent cell the indexes are equal.
+        if position == index {
+            return (parent, position == index, position)
+        }
+        
+        item = self.dataSource[parent - 1]
+        return (parent - 1, position == index, position - item.childs.count - 1)
+    }
+}
+
+extension AccordionTableViewController {
+    
+    // MARK: UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.total
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell : UITableViewCell!
+        
+        let (parent, isParentCell, actualPosition) = self.findParent(indexPath.row)
+        
+        if !isParentCell {
+            cell = tableView.dequeueReusableCellWithIdentifier(childCellIdentifier, forIndexPath: indexPath)
+            cell.textLabel!.text = self.dataSource[parent].childs[indexPath.row - actualPosition - 1]
+        }
+        else {
+            cell = tableView.dequeueReusableCellWithIdentifier(parentCellIdentifier, forIndexPath: indexPath)
+            cell.textLabel!.text = self.dataSource[parent].title
+        }
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let (parent, isParentCell, actualPosition) = self.findParent(indexPath.row)
+        
+        guard isParentCell else {
+            NSLog("A child was tapped!!!")
+            
+            // The value of the child is indexPath.row - actualPosition - 1
+            NSLog("The value of the child is \(self.dataSource[parent].childs[indexPath.row - actualPosition - 1])")
+            
+            return
+        }
+        
+        self.tableView.beginUpdates()
+        self.updateCells(parent, index: indexPath.row)
+        self.tableView.endUpdates()
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return !self.findParent(indexPath.row).isParentCell ? 44.0 : 64.0
+    }
+}
--- AccordionTableSwift/SampleTableViewController.swift
+++ AccordionTableSwift/SampleTableViewController.swift
@@ -8,88 +8,25 @@
 
 import UIKit
 
-class SampleTableViewController: UITableViewController {
-
+class SampleTableViewController: AccordionTableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let item1 = Parent(state: .Collapsed, childs: ["SubItem 1", "SubItem 2", "SubItem 3"], title: "Item 1")
+        let item2 = Parent(state: .Collapsed, childs: ["SubItem 1", "SubItem 2"], title: "Item 2")
+        let item3 = Parent(state: .Collapsed, childs: ["SubItem 1", "SubItem 2", "SubItem 3"], title: "Item 3")
+        let item4 = Parent(state: .Collapsed, childs: ["SubItem 1", "SubItem 2"], title: "Item 4")
+        let item5 = Parent(state: .Collapsed, childs: ["SubItem 1", "SubItem 2"], title: "Item 5")
+        
+        self.dataSource = [item1, item2, item3, item4, item5]
+        self.total = dataSource.count
+        self.numberOfCellsExpanded = .Several
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
