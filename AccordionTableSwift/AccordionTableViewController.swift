66adec8fde5691f5a0f2f2d9c2248da194145d29
Victor Sigler <vikt0r.sigler@gmail.com>
2016-07-05 18:05:47 -0400

    Change the style of the cells

---
+++ AccordionTableSwift/AccordionTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AccordionTableViewController.swift
+//  AccordionTableSwift
+//
+//  Created by Victor on 7/5/16.
+//  Copyright © 2016 Pentlab. All rights reserved.
+//
+
+import Foundation
