0a15a4e38e6398758a9a99cde35c7b22d780e00e
Victor Sigler <vsiglerlopez@e-builder.net>
2016-11-18 23:15:23 -0500

    Add the build for the new branch to test

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+
+if [[ "${TRAVIS_BRANCH}" == "swift-3"]]; then
+   gem install bundler
+   bundle install
+   bundle exec fastlane test
+   exit $?
+fi
+
 if [[ "TRAVIS_PULL_REQUEST" != false]]; then
    gem install bundler
    bundle install
