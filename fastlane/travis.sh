be39d75f0dbf3d005ce5692ce355d0e41a8fda40
Victor Sigler <vsiglerlopez@e-builder.net>
2016-11-18 23:23:51 -0500

    Update script

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
+bundle install
+bundle exec fastlane test
+exit $?
 
-
-if [[ "${TRAVIS_BRANCH}" == "swift-3"]]; then
-   gem install bundler
-   bundle install
-   bundle exec fastlane test
-   exit $?
-fi
-
-if [[ "TRAVIS_PULL_REQUEST" != false]]; then
-   gem install bundler
-   bundle install
-   bundle exec fastlane test
-   exit $?
-fi
+#if [[ "TRAVIS_PULL_REQUEST" != false]]; then
+#   gem install bundler
+#   bundle install
+#   bundle exec fastlane test
+#   exit $?
+#fi
