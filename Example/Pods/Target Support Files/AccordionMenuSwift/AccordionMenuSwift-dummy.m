6129c488c790b1b8d95c824ca8e7ce0f606940de
Victor Sigler <vsiglerlopez@e-builder.net>
2016-11-20 12:31:24 -0500

    Add Cocoapods support for the project

---
+++ Example/Pods/Target Support Files/AccordionMenuSwift/AccordionMenuSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AccordionMenuSwift : NSObject
+@end
+@implementation PodsDummy_AccordionMenuSwift
+@end
---
+++ Example/Pods/Target Support Files/Pods-AccordionMenuSwift_Example/Pods-AccordionMenuSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AccordionMenuSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AccordionMenuSwift_Example
+@end
